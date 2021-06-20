e90a9070f86f32a6a49dbc4db95ed03e2aba8277
Akmaral <1raposa@bk.ru>
2021-06-20 21:32:46 +0500

    uslovia v js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,34 @@
+"use strict";
 
-/* Задание на урок:
+if (4 == 9){
+    console.log('OK');
+} else {
+ console.log('error');
+}
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+const num = 50;
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+// if (num<49){
+//     console.log('error');
+// } else if (num>100) {
+//     console.log('many');
+// } else {
+//     console.log('ok');
+// }
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+// (num === 50) ? console.log('ok') : console.log('error');
 
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-            
-const firstQ = prompt('Один из последних просмотренных фильмов?', ''),
-      secondQ = prompt('На сколько оцените его?', ''),         
-      thirdQ = prompt('Один из последних просмотренных фильмов?', ''),
-      forthQ = prompt('На сколько оцените его?', ''); 
-
-personalMovieDB.movies[firstQ] = secondQ;
-personalMovieDB.movies[thirdQ] = forthQ;
-
-console.log(personalMovieDB);
+switch (num) {
+    case 49:
+        console.log('error');
+        break;
+    case 100:
+        console.log('error');
+        break;
+    case 50: 
+        console.log('ok');
+        break;
+    default: 
+        console.log('эх');
+        break;
+}
