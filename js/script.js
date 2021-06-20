99940dc7cc6920868bfc1c1383f1ad53ef5e5892
Akmaral <1raposa@bk.ru>
2021-06-20 21:45:59 +0500

    cycles

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
 "use strict";
 
-if (4 == 9){
-    console.log('OK');
-} else {
- console.log('error');
-}
+let num = 50;
 
-const num = 50;
-
-// if (num<49){
-//     console.log('error');
-// } else if (num>100) {
-//     console.log('many');
-// } else {
-//     console.log('ok');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log('ok') : console.log('error');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
 
-switch (num) {
-    case 49:
-        console.log('error');
-        break;
-    case 100:
-        console.log('error');
-        break;
-    case 50: 
-        console.log('ok');
-        break;
-    default: 
-        console.log('эх');
-        break;
+for (let i = 1; i < 10; i++) {
+    if(i===6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
