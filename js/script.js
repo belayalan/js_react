f500a23bdf86be4884db6282e6dd2a1829ea00df
Akmaral <1raposa@bk.ru>
2021-07-05 18:53:10 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
-"use strict";
+'use strcit';
 
-let num = 50;
+const numberOfFilms = prompt('Сколько фильмов посмотрели', '');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
 
-for (let i = 1; i < 10; i++) {
-    if(i===6) {
-        // break;
-        continue;
-    }
-    console.log(i);
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из послед фильм', ''),
+          b = prompt('на скок оценишь', '');
+      
+if (a != null && b !=null && a != '' && b !='' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+}
+
+if (personalMovieDB.count < 10){
+    console.log('просмотрено довольно мало фильмецов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('средне');
+} else if (personalMovieDB.count >= 30) {
+    console.log('kinoman');
+} else {
+    console.log('error');
+}
 }
