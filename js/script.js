d88336ba2de85ca654360098460dded0db87757f
Akmaral <1raposa@bk.ru>
2021-07-05 22:49:03 +0500

    func

--- js/script.js
+++ js/script.js
@@ -1,35 +1,26 @@
 'use strcit';
+let num = 20;
 
-const numberOfFilms = prompt('Сколько фильмов посмотрели', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-for (let i = 0; i < 2; i++){
-    const a = prompt('Один из послед фильм', ''),
-          b = prompt('на скок оценишь', '');
-      
-if (a != null && b !=null && a != '' && b !='' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log('done');
-} else {
-    console.log('error');
-    i--;
+function showFirstMessage(text) {
+ console.log(text);
+ let num = 10;
 }
 
-if (personalMovieDB.count < 10){
-    console.log('просмотрено довольно мало фильмецов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('средне');
-} else if (personalMovieDB.count >= 30) {
-    console.log('kinoman');
-} else {
-    console.log('error');
+showFirstMessage('hello world!');
+console.log(num);
+
+
+function calc (a, b) {
+    return (a+b);
 }
+
+console.log(calc(4, 3));
+
+function retur() {
+    let num = 50;
+    return num;
+}
+
+
+let foo = function (){
 }
