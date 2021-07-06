b55155c37b6d273d7f66ee697c3d3e6becaf75e0
Akmaral <1raposa@bk.ru>
2021-07-06 18:21:53 +0500

    3prac

--- js/script.js
+++ js/script.js
@@ -1,26 +1,67 @@
-'use strcit';
-let num = 20;
+'use strict';
 
-function showFirstMessage(text) {
- console.log(text);
- let num = 10;
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
+    }
 }
 
-showFirstMessage('hello world!');
-console.log(num);
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-function calc (a, b) {
-    return (a+b);
+ function rememberMyFilms() {
+   for (let i = 0; i < 2; i++){
+     const a = prompt('Один из послед фильм', ''),
+           b = prompt('на скок оценишь', '');
+   }
+   if (a != null && b !=null && a != '' && b !='' && a.length < 50) {
+     personalMovieDB.movies[a] = b;
+     console.log('done');
+          } else {
+     console.log('error');
+     i--;
+   }
+ }
+
+// rememberMyFilms();
+
+
+function detectPersonalLevel () {
+  if (personalMovieDB.count < 10){
+    console.log('просмотрено довольно мало фильмецов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('средне');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('kinoman');
+  } else {
+    console.log('error');
+  }
 }
+detectPersonalLevel ();
 
-console.log(calc(4, 3));
-
-function retur() {
-    let num = 50;
-    return num;
+function showMyDB (hidden) {
+  if (!hidden){
+     console.log(personalMovieDB);
+  }
 }
+showMyDB(personalMovieDB.privat);
 
-
-let foo = function (){
+function writeYourGenres() {
+   for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+   }
 }
+writeYourGenres();
+
