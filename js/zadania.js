204eff2336e81515eb3fecbcadbeab01e58a558c
Akmaral <1raposa@bk.ru>
2021-07-10 15:36:22 +0500

    OOP

--- js/zadania.js
+++ js/zadania.js
@@ -1,86 +1,19 @@
 'use strict';
 
-// const obj = {
-//     a: 5,
-//     b: 1
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+const john = Object.create(soldier); //создание нового обхекта джон который будетпрототипно наследоваться от солдата
+
+// const john = {
+//     health: 100
 // };
 
-// const copy = obj; //ссылка на обж
+// Object.setPrototypeOf(john, soldier); //получение прототип для джона
 
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy (mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 4,
-    c: {
-        x: 7,
-        y: 3
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-// console.log(newNumbers);
-// console.log(numbers);
-
-
-const add = {
-    d: 15,
-    e: 3
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'azzzzzzzzzz';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'tiktok', 'rutube'],
-        blogs = ['wordpress', 'livej', 'blogger'],
-        internet = [...video, ...blogs, 'vk'];
-
-        console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-     console.log(b);
-      console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newRray = [...array];
-
-const q = {
-    one: 1,
-    two: 2 
-};
-
-const newObj = {...q};
+john.sayHello();
