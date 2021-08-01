7fa6c79f1ff406281689b050cbc180515b8fd317
Akmaral <1raposa@bk.ru>
2021-08-01 18:53:23 +0500

    mob

--- js/script.js
+++ js/script.js
@@ -1,45 +1,23 @@
 'use strict';
 
-      const box = document.getElementById('box');
-      const buttons = document.getElementsByTagName('button');
-      const circles = document.getElementsByClassName('circle');
-      const wrapper = document.querySelector('.wrapper');
-      const hearts = document.querySelectorAll('.heart');
-      const oneHeart = document.querySelector('.heart');
-      
+//touchstart начало пальца
+// touch move движение пальца
+// touchend
+// touchenter как только палец зашел на элемент
+// touchlevae продолжил скользить палец за пределы элемента
+// touchcancel точка соприкосновения больше не регистрируется
 
-            // box.style.backgroundColor = 'blue';
-            // box.style.width = '500px';
-            box.style.cssText = `background-color: blue; width: 500px`;
+window.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.box');
 
-            buttons[1].style.borderRadius = '100%';
-            circles[0].style.backgroundColor = 'red';
+  box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
 
-            // for (let i = 0; i < hearts.length; i++) {
-            //       hearts[i].style.backgroundColor = 'pink';
-            // }
-      
-            hearts.forEach(item => {
-                  item.style.backgroundColor = 'pink';
-            });
+       
+        console.log(e.targetTouches[0].pageX);
+  });
+});
 
-            const div = document.createElement('div');
-            // const text = document.createTextNode('Hello world');
-
-            div.classList.add('black');
-
-            wrapper.append(div);  //в конец
-            // wrapper.appendChild(div);
-            // wrapper.prepend(div)
-
-            // hearts[0].after(div)
-            // wrapper.insertBefore(div, hearts[0])
-            // circles[1].remove();
-
-            // hearts[0].replaceWith(circles[0]); // замена элем
-
-            div.innerHTML = '<h1> hello world! </h1>';
-
-            // div.textContent = 'hello';
-
-            div.insertAdjacentHTML('afterend', '<h1> hello world! </h1>');
+//touches колво пальцев
+// targettouches
+// changeTouches
